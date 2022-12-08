81eec7f864a3ef7da5fe7e8eab1c63b928c61377
Niffe <petrovnik01@ukr.net>
2022-12-08 14:16:57 +0200

    Add empty project

---
+++ Module3HW4/Module3HW4-delegates/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module3HW4_delegates
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
